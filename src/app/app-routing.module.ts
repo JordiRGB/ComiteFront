7c9c0ad3018be53ace7e49e6314e0ec80d327b0e
JordiRGB <jordiraf.07@gmail.com>
2023-11-20 22:18:10 -0600

    acomodo de login y reestablecer, formulario responsivo, tal vez se cambie.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PagInicioComponent } from './componentes/pag-inicio/pag-inicio.component';
 import { PagRegCasoComponent } from './componentes/pag-reg-caso/pag-reg-caso.component';
+import { PagLogInComponent } from './componentes/pag-log-in/pag-log-in.component';
+import { PagRestablecerPassComponent } from './componentes/pag-restablecer-pass/pag-restablecer-pass.component';
 
 const routes: Routes = [
   { path: 'inicio', component: PagInicioComponent },
   { path: 'regcaso', component: PagRegCasoComponent },
+  { path: 'login', component: PagLogInComponent },
+  { path: 'restablecer', component: PagRestablecerPassComponent },
   { path: '**', component: PagInicioComponent } // Manejo de rutas no encontradas
 ];
 
---
+++ src/app/componentes/pag-log-in/pag-log-in.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pag-log-in',
+  templateUrl: './pag-log-in.component.html',
+  styleUrls: ['./pag-log-in.component.css']
+})
+export class PagLogInComponent {
+
+}
