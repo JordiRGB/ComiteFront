0ad96438261c739b43c5b2337bbf7449c81aaa03
JordiRGB <jordiraf.07@gmail.com>
2023-11-29 22:56:30 -0600

    resolviendo errores de combinacion y subida de cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,9 @@ import { PagLogInComponent } from './componentes/pag-log-in/pag-log-in.component
 import { PagRestablecerPassComponent } from './componentes/pag-restablecer-pass/pag-restablecer-pass.component';
 import {PagSignUpComponent}from './componentes/pag-sign-up/pag-sign-up.component'
 import { JefeiscComponent } from './componentes/jefeisc/jefeisc.component';
+import { PapeleraComponent } from './componentes/papelera/papelera.component';
+import { AdministradorComponent } from './componentes/administrador/administrador.component';
+
 
 const routes: Routes = [
   { path: 'inicio', component: PagInicioComponent },
@@ -14,7 +17,9 @@ const routes: Routes = [
   { path: 'signup', component: PagSignUpComponent},
   { path: 'restablecer', component: PagRestablecerPassComponent },
   { path: '**', component: PagInicioComponent }, // Manejo de rutas no encontradas
-  
+  { path: 'jefesisc', component: JefeiscComponent},
+  { path: 'papelera', component: PapeleraComponent},
+  { path: 'administrador', component: AdministradorComponent},
 ];
 
 @NgModule({  
