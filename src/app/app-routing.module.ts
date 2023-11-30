578d5f8162b6aa94959141d0f3660cdbb4be71cd
Robertobg01 <127140945+Robertobg01@users.noreply.github.com>
2023-11-29 21:51:28 -0600

    Acomodando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PagInicioComponent } from './componentes/pag-inicio/pag-inicio.component';
 import { JefeiscComponent } from './componentes/jefeisc/jefeisc.component';
+import { PapeleraComponent } from './componentes/papelera/papelera.component';
+import { AdministradorComponent } from './componentes/administrador/administrador.component';
 
 const routes: Routes = [
-  { path: '', component: PagInicioComponent }
+  { path: 'inicio', component: PagInicioComponent },
+  { path: 'jefeisc', component: JefeiscComponent},
+  { path: 'papelera', component: PapeleraComponent},
+  { path: 'administrador', component: AdministradorComponent}
 ];
 
 @NgModule({  
